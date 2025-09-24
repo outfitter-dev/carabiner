6f8dfd9f7d21b7003f5d8511b0723509f1f3a14d
Matt Galligan <matt.galligan@gmail.com>
2025-09-24 13:06:01 -0400

    feat: add context factories with environment variable injection

---
+++ packages/hooks-core/src/context/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Context creation utilities for Claude Code hooks
+ */
+
+export {
+  clearHookEnvironment,
+  getHookEnvironment,
+  injectEnvironmentVariables,
+  validateHookEnvironment,
+} from "../environment";
+export * from "./factory";
